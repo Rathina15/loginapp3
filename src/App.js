990006a7443c7876d2fd42800c8f0ca07ce0e9ce
Rathina <rathina010@gmail.com>
2022-01-11 21:52:40 +0530

    commit new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 import {Login2} from './login2';
 import {Login3} from './login3';
-import {Login4} from './login4';
+
 function App() {
   return (
+  
    <div className='App'>
      <center>
     <Login2 /> 
     <div >
    <Login3 />
     </div>
-   <div>
-   <Login4 /> 
-   </div>
    </center>
    </div>
    
--- src/login3.js
+++ src/login3.js
@@ -5,6 +5,10 @@ export function Login3() {
 <div>Login Username </div>
 <h2>USERNAME:</h2> <input type='text' placeholder='Enter your name'></input><br></br>
      <h2>PASSWORD:</h2> <input type='password' placeholder='Enter the password'></input><br></br>
+     <div>Login button</div>
+<h1><button onclick ="alert('Successfully Logined!')">Login</button><br></br></h1>
+
+
      
      </div>);
      
